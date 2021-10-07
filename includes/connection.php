ae2ccd682c192d20f584a7439ed35f217058eded
Karan Shetty <shettykaran2110@gmail.com>
2021-10-07 18:57:20 +0530

    Extra files removed

---
+++ includes/connection.php
@@ -0,0 +1,4 @@
+<?php
+ $db = mysqli_connect('localhost', 'root', '') or
+        die ('Unable to connect. Check your connection parameters.');
+        mysqli_select_db($db, 'dbms_project' ) or die(mysqli_error($db));
